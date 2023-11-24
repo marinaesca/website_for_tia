21aa1803a5c1d8c639913d55521fea76d9f6f587
marinaesca <marinaescalante@utexas.edu>
2023-11-23 22:38:18 -0700

    initial set up of front end with create react app

---
+++ api/frontend/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">How exciting!</header>
+    </div>
+  );
+}
+
+export default App;
